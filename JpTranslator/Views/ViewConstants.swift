098b30c8317dc931a36c7726dcdf12dd2c51c189
buddhika <buddhika83@gmail.com>
2020-04-17 08:38:54 +0900

    remove unwanted code

---
+++ JpTranslator/Views/ViewConstants.swift
@@ -0,0 +1,24 @@
+//
+//  ViewConstants.swift
+//  JpTranslator
+//
+//  Created by Buddhika Pallewela on 2020/04/17.
+//  Copyright © 2020 Buddhika Pallewela. All rights reserved.
+//
+
+import UIKit
+
+/// Color Constants
+struct ViewColor{
+    static let basicBorder = UIColor.purple
+    static let basicText = UIColor.purple
+    static let basicButtonBackground = UIColor.purple
+    static let basicButtonText = UIColor.yellow
+    static let defaultBackground = UIColor.lightGray
+}
+
+/// Color Constants
+struct ViewProperties{
+    static let cornerRadius: CGFloat = 5.0
+    static let borderWidth: CGFloat = 1
+}
